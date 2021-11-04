708d83c39a08916e2a2fb0df6c17ded7ebaa603f
Aditya Dhayfule <adityadhayfule3101@gmail.com>
2021-11-04 17:31:47 +0530

    updated some cards

--- aspirine/src/components/logins.js
+++ aspirine/src/components/logins.js
@@ -1,18 +1,24 @@
 import React from "react";
+import { MdBloodtype } from "react-icons/md";
 import { BiFirstAid } from "react-icons/bi";
 
 const Logins = () => {
   return (
-    <>
+    <section className="login-container">
       <div className="loginText">
         Hello User kindly select Proper Login option
       </div>
       <section class="section-container">
         <div className="hospital">
           <BiFirstAid className="first-aid-logo" />
+          <p>Hospital Login</p>
+        </div>
+        <div className="blood-bank">
+          <MdBloodtype className="blood-bank-logo" />
+          <p>Blood Bank</p>
         </div>
       </section>
-    </>
+    </section>
   );
 };
 
